b9c2dc0684ddcd42f668e8ed361233f87420c671
vasko34 <56612508+vasko34@users.noreply.github.com>
2023-11-18 16:01:07 +0200

    c

--- Project18/ViewController.swift
+++ Project18/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  Project18
-//
-//  Created by User on 5.11.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("in viewDidLoad")
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("something", terminator: "") // usually terminator is "\n"
+        
+        assert(1 == 1, "math failed")
+        assert(1 == 2, "math did not fail")
+        assert(mySlowFunc() == true, "The mySlowFunc() returned false, which is bad")
+        
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
     }
-
-
+    
+    func mySlowFunc() -> Bool {
+        for i in 0...1000000000 {
+            for j in 0...1000000000 {
+                print(i * i + j * j)
+            }
+        }
+        
+        if Int.random(in: 1...2) == 1 {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
